6d6b4e4b058106c1aca7f32841c588c685f5b5f8
FarhodD <farhoddavlatov0120@gmail.com>
2025-02-23 14:00:51 +0500

    feat: добавлены плагины HtmlWebpackPlugin и CopyWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -8,4 +11,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'public/assets'), to: 'assets' },
+        { from: path.resolve(__dirname, 'public/fonts'), to: 'fonts' },
+      ],
+    }),
+  ]
 }
